a76a3bdd6ce20a47a106a16c2919b5717d69af12
Ярослав Болохонов <jroslavl.2005@gmail.com>
2025-06-04 19:01:24 +0300

    done all changed

--- ParallelLab/main.cpp
+++ ParallelLab/main.cpp
@@ -14,10 +14,11 @@
 #include <random>
 #include <omp.h>
 
-constexpr size_t COLS = 5; // Количество столбцов (размер блока)
-constexpr size_t ROWS = 15;  // Количество строк
-constexpr size_t MATR_SIZE = COLS * ROWS; // Размер матрицы
-constexpr size_t NTHREADS = 4; //Кол-во потоков (распараллеливание по строкам)
+constexpr const size_t COLS = 5; // Количество столбцов (размер блока)
+constexpr const size_t ROWS = 15;  // Количество строк
+constexpr const size_t MATR_SIZE = COLS * ROWS; // Размер матрицы
+constexpr const size_t NTHREADS = 4; //Кол-во потоков (распараллеливание по строкам)
+constexpr const size_t BLOCK = ROWS / NTHREADS;
 
 struct INFORM
 {
@@ -101,7 +102,7 @@ bool is_all_even(int* arr)
 
 
 
-// task 1
+ // task 1
 unsigned __stdcall is_all_even_parallel(void* arg) // one thread
 {
 	INFORM* inform = (INFORM*)arg;
@@ -112,30 +113,27 @@ unsigned __stdcall is_all_even_parallel(void* arg) // one thread
 			count++;
 	}
 	inform->cnt = count;
-	if (inform->right != ROWS - 1)
-		_endthreadex(0);
 	return 0;
 }
 
 int all_even_rows_count_parallel(int** matrix)
 {
 	HANDLE t[NTHREADS - 1];
-	size_t block = ROWS / NTHREADS; // Размер блока для каждого потока
 	INFORM inform[NTHREADS];
 	for (size_t i{}; i < NTHREADS; ++i)
 	{
 		inform[i].matrix = matrix;
-		inform[i].left = i * block;
+		inform[i].left = i * BLOCK;
 		if (i == NTHREADS - 1)
-			inform[i].right = ROWS - 1;
+			inform[i].right = ROWS;
 		else
 		{
-			inform[i].right = (i + 1) * block;
+			inform[i].right = (i + 1) * BLOCK;
 			t[i] = (HANDLE)_beginthreadex(nullptr, 0, &is_all_even_parallel, inform + i, 0, nullptr);
 		}
 	}
 	is_all_even_parallel(&inform[NTHREADS - 1]); // Последний поток обрабатывает оставшиеся строки
-
+	
 	WaitForMultipleObjects(NTHREADS - 1, t, true, INFINITE);
 
 	int count{};
@@ -149,205 +147,249 @@ int all_even_rows_count_parallel(int** matrix)
 }
 
 // task 2
-void is_all_even_ref(int* arr, bool& result)
+int all_even_rows_count_ref(int** matrix, size_t left, size_t right, int& count)
 {
-	result = true;
-	for (size_t i{}; i < COLS && result; ++i)
-		if (arr[i] % 2 != 0)
-			result = false;
+	count = 0;
+	for (size_t i = left; i < right; ++i)
+	{
+		bool result = true;
+		int j{};
+		while (j < COLS && result)
+		{
+			if (matrix[i][j] % 2 != 0)
+				result = false;
+			j++;
+		}
+		if (result)
+			count++;
+	}
+	return count;
 }
 
 int all_even_rows_count_parallel2(int** matrix)
 {
-	std::thread t[NTHREADS];
-	bool results[NTHREADS];
-
-	for (size_t i{}; i < NTHREADS; ++i)
+	std::thread t[NTHREADS - 1];
+	int results[NTHREADS - 1];
+	
+	for (size_t i{}; i < NTHREADS - 1; ++i)
 	{
-		int* row = matrix[i];
-		t[i] = std::thread(is_all_even_ref, row, std::ref(results[i]));
+		t[i] = std::thread(all_even_rows_count_ref, matrix, i * BLOCK, (i + 1) * BLOCK, std::ref(results[i]));
 	}
 
-	for (size_t i = 0; i < NTHREADS; ++i)
-		t[i].join();
-
 	int count{};
-	for (size_t i = 0; i < NTHREADS; ++i)
-		if (results[i])
-			count++;
+	all_even_rows_count_ref(matrix, (NTHREADS - 1) * BLOCK, ROWS, count); // Главный поток обрабатывает оставшиеся строки
+
+	for (size_t i = 0; i < NTHREADS - 1; ++i) // sync
+		t[i].join(); 
+
+	for (size_t i = 0; i < NTHREADS - 1; ++i)
+		count += results[i];
 
 	return count;
 }
 
-//// task 3
-//int all_even_rows_count_parallel3(int** matrix)
-//{
-//	std::future<bool> t[NTHREADS];
-//	for (size_t i{}; i < NTHREADS; ++i)
-//	{
-//		int* row = matrix[i];
-//		t[i] = std::async(std::launch::async, is_all_even, row);
-//	}
-//
-//	int count{};
-//	for (size_t i{}; i < NTHREADS; ++i)
-//		if (t[i].get())
-//			count++;
-//	return count;
-//}
-//
-//// task 4 (atomic)
-//void is_all_even_atomic(int* arr, std::atomic_bool& result)
-//{
-//	result = true;
-//	for (size_t i{}; i < COLS && result; ++i)
-//		if (arr[i] % 2 != 0)
-//			result = false;
-//}
-//
-//int all_even_rows_count_parallel4(int** matrix)
-//{
-//	std::thread t[NTHREADS];
-//	std::atomic_bool results[NTHREADS];
-//
-//	for (size_t i{}; i < NTHREADS; ++i)
-//	{
-//		int* row = matrix[i];
-//		t[i] = std::thread(is_all_even_atomic, row, std::ref(results[i]));
-//	}
-//
-//	for (size_t i = 0; i < NTHREADS; ++i)
-//		t[i].join();
-//
-//	int count{};
-//	for (size_t i = 0; i < NTHREADS; ++i)
-//		if (results[i])
-//			count++;
-//	return count;
-//}
-//
-//// task 5 (Queue, Mutex C++)
-//ThreadsafeQueue<int> que_int;
-//
-//void is_all_even_queue(int* arr, std::atomic_bool& result)
-//{
-//	int row{};
-//	if (que_int.try_pop(row)) // извлекаем индекс строки из очереди
-//	{
-//		result = true;
-//		for (size_t i{}; i < COLS && result; ++i)
-//			if (arr[i] % 2 != 0)
-//				result = false;
-//	}
-//	else
-//		result = false;
-//}
-//
-//int all_even_rows_count_parallel5(int** matrix)
-//{
-//	std::thread t[NTHREADS];
-//	std::atomic_bool results[NTHREADS];
-//
-//	// Заполняем очередь индексами строк
-//	for (size_t i{}; i < NTHREADS; ++i)
-//		que_int.push(i);
-//
-//	for (size_t i{}; i < NTHREADS; ++i)
-//	{
-//		int* row = matrix[i];
-//		t[i] = std::thread(is_all_even_queue, row, std::ref(results[i]));
-//	}
-//
-//	for (size_t i = 0; i < NTHREADS; ++i)
-//		t[i].join();
-//
-//	int count{};
-//	for (size_t i = 0; i < NTHREADS; ++i)
-//		if (results[i])
-//			count++;
-//	return count;
-//}
-//
-//// task 6
-//volatile long volume_work_producer = ROWS;
-//volatile long volume_work_consumer = ROWS;
-//std::mutex mut;
-//std::condition_variable cv;
-//ThreadsafeQueue<int*> que_ptr_int;
-//
-//void task_producer()
-//{
-//    std::random_device rd;
-//    std::mt19937 gen(rd());
-//    std::uniform_int_distribution<int> dist(0, 50); // 0..50, чтобы после *2 не выйти за 100
-//
-//    while (_InterlockedExchangeAdd(&volume_work_producer, -1) > 0)
-//    {
-//    int* row = new int[COLS];
-//    for (size_t i{}; i < COLS; ++i)
-//    {
-//    row[i] = dist(gen) * 2; // генерируем только чётные числа
-//    std::cout << row[i] << ' ';
-//    std::this_thread::sleep_for(std::chrono::milliseconds(200));
-//    }
-//    std::cout << '\n';
-//    que_ptr_int.push(row);
-//    cv.notify_all();
-//    }
-//
-//	while (_InterlockedExchangeAdd(&volume_work_producer, -1) > 0)
-//	{
-//		int* row = new int[COLS];
-//		for (size_t i{}; i < COLS; ++i)
-//		{
-//			row[i] = dist(gen);
-//			std::cout << row[i] << ' ';
-//			std::this_thread::sleep_for(std::chrono::milliseconds(200));
-//		}
-//		std::cout << '\n';
-//		que_ptr_int.push(row);
-//		cv.notify_all();
-//	}
-//}
-//
-//void task_consumer(int& count)
-//{
-//	count = 0;
-//	bool all_even{};
-//
-//	while (_InterlockedExchangeAdd(&volume_work_consumer, -1) > 0)
-//	{
-//		int* row = nullptr;
-//		std::unique_lock<std::mutex> locker(mut);
-//		cv.wait_for(locker, std::chrono::seconds(5), []() {return !que_ptr_int.empty(); });
-//		if (que_ptr_int.try_pop(row))
-//		{
-//			std::this_thread::sleep_for(std::chrono::milliseconds(400));
-//			all_even = is_all_even(row);
-//			std::cout << std::boolalpha << all_even << '\n';
-//			if (all_even)
-//				count++;
-//			delete[] row;
-//		}
-//		else
-//		{
-//			_InterlockedExchangeAdd(&volume_work_consumer, 1);
-//		}
-//	}
-//}
-//
-//// task 7 (OpenMP)
-//int all_even_rows_count_parallel7(int** matrix)
-//{
-//	int count{};
-//	#pragma omp parallel for reduction(+:count)
-//	for (size_t i{}; i < ROWS; ++i)
-//	{
-//		if (is_all_even(matrix[i]))
-//			count++;
-//	}
-//	return count;
-//}
+int all_even_rows_count_parallel3(int** matrix)  
+{  
+    std::future<int> t[NTHREADS - 1];  
+
+    for (size_t i{}; i < NTHREADS - 1; ++i)  
+    {  
+        t[i] = std::async(std::launch::async, all_even_rows_count, matrix, i * BLOCK, (i + 1) * BLOCK);  
+    }  
+
+    int count{};  
+    count += all_even_rows_count(matrix, (NTHREADS - 1) * BLOCK, ROWS); // Главный поток обрабатывает оставшиеся строки  
+
+    for (size_t i{}; i < NTHREADS - 1; ++i)  
+    {  
+        count += t[i].get();  
+    }  
+
+    return count;  
+}
+
+// task 4 (atomic)
+int all_even_rows_count_atomic(int** matrix, size_t left, size_t right, std::atomic_int& count)
+{
+	count = 0;
+	for (size_t i = left; i < right; ++i)
+	{
+		bool result = true;
+		int j{};
+		while (j < COLS && result)
+		{
+			if (matrix[i][j] % 2 != 0)
+				result = false;
+			j++;
+		}
+		if (result)
+			count++;
+	}
+	return count;
+}
+
+int all_even_rows_count_parallel4(int** matrix)
+{
+	std::thread t[NTHREADS - 1];
+	std::atomic_int results[NTHREADS - 1];
+
+	for (size_t i{}; i < NTHREADS - 1; ++i)
+	{
+		t[i] = std::thread(all_even_rows_count_atomic, matrix, i * BLOCK, (i + 1) * BLOCK, std::ref(results[i]));
+	}
+
+	for (size_t i = 0; i < NTHREADS - 1; ++i)
+		t[i].join();
+
+	int count{};
+	count += all_even_rows_count(matrix, (NTHREADS - 1) * BLOCK, ROWS); // Главный поток обрабатывает оставшиеся строки
+
+	for (size_t i = 0; i < NTHREADS - 1; ++i)
+		count += results[i];
+
+	return count;
+}
+
+// task 5 (Queue, Mutex C++)
+ThreadsafeQueue<std::pair<size_t, size_t>> que_int;
+
+void all_even_rows_count_queue(int** matrix, std::atomic_int& result)
+{
+	std::pair<size_t, size_t> pos{};
+	if (que_int.try_pop(pos)) // извлекаем индекс строки из очереди
+	{
+		result += all_even_rows_count(matrix, pos.first, pos.second);
+	}
+}
+
+int all_even_rows_count_parallel5(int** matrix)
+{
+	std::thread t[NTHREADS - 1];
+	std::atomic_int results[NTHREADS - 1];
+
+	// Заполняем очередь
+	for (size_t i{}; i < NTHREADS - 1; ++i)
+		que_int.push(std::pair<size_t, size_t>(i * BLOCK, (i + 1) * BLOCK));
+
+	for (size_t i{}; i < NTHREADS - 1; ++i)
+	{
+		t[i] = std::thread(all_even_rows_count_queue, matrix, std::ref(results[i]));
+	}
+
+	for (size_t i = 0; i < NTHREADS - 1; ++i)
+		t[i].join();
+
+	int count{};
+	count += all_even_rows_count(matrix, (NTHREADS - 1) * BLOCK, ROWS); // Главный поток обрабатывает оставшиеся строки
+
+	for (size_t i = 0; i < NTHREADS - 1; ++i)
+		count += results[i];
+	return count;
+}
+
+// task 6
+volatile long volume_work_producer = ROWS;
+volatile long volume_work_consumer = ROWS;
+std::mutex mut;
+std::condition_variable cv;
+ThreadsafeQueue<int*> que_ptr_int;
+
+void task_producer()
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(0, 50); // 0..50, чтобы после *2 не выйти за 100
+
+	int row_index{};
+    while (_InterlockedExchangeAdd(&volume_work_producer, -1) > 0)
+    {
+		int* row = new int[COLS];
+		for (size_t i{}; i < COLS; ++i)
+		{
+			if (row_index % 2 == 0) // чётные строки
+				row[i] = dist(gen) * 2; // генерируем только чётные числа
+			else
+				row[i] = dist(gen); 
+			std::cout << row[i] << ' ';
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		}
+		std::cout << '\n';
+		que_ptr_int.push(row);
+		row_index++;
+		cv.notify_all();
+    }
+
+	while (_InterlockedExchangeAdd(&volume_work_producer, -1) > 0)
+	{
+		int* row = new int[COLS];
+		for (size_t i{}; i < COLS; ++i)
+		{
+			row[i] = dist(gen);
+			std::cout << row[i] << ' ';
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		}
+		std::cout << '\n';
+		que_ptr_int.push(row);
+		cv.notify_all();
+	}
+}
+
+void task_consumer(int& count)
+{
+	count = 0;
+	bool all_even{};
+
+	while (_InterlockedExchangeAdd(&volume_work_consumer, -1) > 0)
+	{
+		int* row = nullptr;
+		std::unique_lock<std::mutex> locker(mut);
+		cv.wait_for(locker, std::chrono::seconds(5), []() {return !que_ptr_int.empty(); });
+		if (que_ptr_int.try_pop(row))
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(400));
+			all_even = is_all_even(row);
+			std::cout << std::boolalpha << all_even << '\n';
+			if (all_even)
+				count++;
+			delete[] row;
+		}
+		else
+		{
+			_InterlockedExchangeAdd(&volume_work_consumer, 1);
+		}
+	}
+}
+
+// task 7 (OpenMP)
+int all_even_rows_count_parallel7(int** matrix)
+{
+	int global_cnt{};
+#pragma omp parallel shared(matrix)
+	{
+		int local_cnt{};
+#pragma omp for
+		{
+			for (size_t i = 0; i < ROWS; ++i)
+			{
+				bool result = true;
+				int j{};
+				while (j < COLS && result)
+				{
+					if (matrix[i][j] % 2 != 0)
+						result = false;
+					j++;
+				}
+				if (result)
+					local_cnt++;
+			}
+		}
+		#pragma omp critical
+		{
+			global_cnt += local_cnt;
+		}
+	}
+	return global_cnt;
+}
 
 
 
@@ -361,12 +403,12 @@ int main()
 
 	std::cout << "Matrix:\n";
 	print_matrix(matrix);
-	std::cout << "\nNon parallel solution\nCount of all-even rows = " << all_even_rows_count(matrix, 0, ROWS - 1) << '\n';
+	std::cout << "\nNon parallel solution\nCount of all-even rows = " << all_even_rows_count(matrix, 0, ROWS) << '\n';
 
 	std::cout << "\nTask 1:\n";
 	std::cout << "Parallel solution = " << all_even_rows_count_parallel(matrix) << '\n';
 
-	/*std::cout << "\nTask 2:\n";
+	std::cout << "\nTask 2:\n";
 	std::cout << "Parallel solution = " << all_even_rows_count_parallel2(matrix) << '\n';
 
 	std::cout << "\nTask 3:\n";
@@ -395,7 +437,7 @@ int main()
 	std::cout << "Parallel solution = " << count << '\n';
 
 	std::cout << "\nTask 7:\n";
-	std::cout << "Parallel solution = " << all_even_rows_count_parallel7(matrix) << '\n';*/
+	std::cout << "Parallel solution = " << all_even_rows_count_parallel7(matrix) << '\n';
 
 	free_memory(matrix);
 
